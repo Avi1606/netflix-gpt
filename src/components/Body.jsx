dfd9a36b18437c9e83c8e09d204c02ead8c8a544
Avikumar <aviiiprajapati16@gmail.com>
2025-04-21 09:53:23 +0530

    componets created

---
+++ src/components/Body.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Login from "./Login.jsx";
+
+
+const Body = () => {
+
+    const appRouter = createBrowserRouter([
+        {
+            path: '/',
+            element: <Login/>
+        },
+
+    ])
+
+    return (
+        <div>
+            <RouterProvider router={appRouter} />
+        </div>
+    );
+};
+
+export default Body;
---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Login = () => {
+
+    return (
+        <div>
+            <img
+                src="https://assets.nflxext.com/ffe/siteui/vlv3/69bec183-9cc8-49d4-8fc2-08228d3c91b4/web/IN-en-20250414-TRIFECTA-perspective_c8273fb1-8860-4ff5-bd1c-c2c4b44d5f2a_large.jpg"
+                alt="logo"/>
+        </div>
+    );
+};
+
+export default Login;
