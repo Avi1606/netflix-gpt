d2eae39af90b564abef7f2b740b2074d992b8ce3
Avikumar <aviiiprajapati16@gmail.com>
2025-04-21 19:29:22 +0530

    midifed code

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
+import validation from "../Utils/Validation.jsx";
+import Validation from "../Utils/Validation.jsx";
 
 const Login = () => {
 
-    const [isSignedIn, setIsSignedIn] = React.useState(true);
+    const [isSignedIn, setIsSignedIn] = useState(true);
 
-    const toogleSignIn = () => {
+    const [error, setError] = useState(null);
+
+    const email = useRef(null);
+    const password = useRef(null);
+
+    const toggleSignIn = () => {
         setIsSignedIn(!isSignedIn);
     }
 
+    const toggleValidation = () =>{
+        //validation
+        const massage =Validation(email.current.value, password.current.value);
+        setError(massage);
+
+    }
+
+
     return (
-        <div className ="realtive">
+        <div className="relative">
 
-          <div className="absolute flex justify-center items-center w-full z-10 inset-0">
-              <form className="bg-black opacity-86 flex flex-col p-12 rounded max-w-md w-full mx-4">
-                  <h2 className="text-white text-2xl font-bold mb-6">{isSignedIn ? "Sign In" :"Sign Up"}</h2>
-                  <input className="bg-gray-650 p-3 my-3  placeholder-white text-white border-1 rounded-2l" type="text" placeholder="Email Address/Number" />
-                  <input className="bg-gray-650 p-3 my-3  placeholder-white text-white border-1 rounded-2l" type="password" placeholder="Password" />
-                  <button className="text-bold bg-red-600 text-white rounded py-3 mt-6 text-sm font-medium">{isSignedIn ? "Sign In" :"Sign Up"}</button>
+            <div className="absolute flex justify-center items-center w-full z-10 inset-0">
+                <form className="bg-black opacity-86 flex flex-col p-15 rounded max-w-md w-full mx-7"
+                      onSubmit={(e) => e.isDefaultPrevented()}>
+                    <h2 className="text-white text-2xl font-bold mb-6">{isSignedIn ? "Sign In" : "Sign Up"}</h2>
+                    {!isSignedIn && (
+                        <input className="bg-gray-950 p-3 my-6  placeholder-white text-white border-1 rounded-2l"
+                               type="text" placeholder="Name"/>)}
+                    <input ref={email}
+                           className="bg-gray-950 p-3 my-3  placeholder-white text-white border-1 rounded-2l "
+                           type="text" placeholder="Email or mobile number"/>
+                    <input ref={password}
+                           className="bg-gray-950 p-3 my-3  placeholder-white text-white border-1 rounded-2l"
+                           type="password" placeholder="Password"/>
+                    <p className="text-red-600">{error}</p>
+                    <button
+                        className="cursor-pointer text-bold bg-red-600 text-white rounded py-3 mt-6 text-sm font-medium rounded-2l"
+                        onClick={toggleValidation}>
+                        {isSignedIn ? "Sign In" : "Sign Up"}</button>
 
-                  <p className ="text-gray-400 mx-1 my-5" >{!isSignedIn ? "Already registered. " :"New to Netflix? "}<span className="text-white cursor-pointer hover:brightness-30" onClick={toogleSignIn}>{isSignedIn ?"Sign In" : "Sign Up"}.</span></p>
-              </form>
-          </div>
+                    <p className="text-gray-400 mx-1 my-9">{!isSignedIn ? "Already registered. " : "New to Netflix ? "}<span
+                        className="text-white cursor-pointer hover:brightness-30"
+                        onClick={toggleSignIn}>{isSignedIn ? "Sign Up" : "Sign In"}.</span></p>
+                </form>
+            </div>
             <div className="">
                 <div className="absolute inset-0 bg-gradient-to-b from-black via-black/10 to-transparent"></div>
                 <img
