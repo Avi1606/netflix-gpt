1c3a8d8eace68f23ebe964ab477b78bc9fc84ee6
Avikumar <aviiiprajapati16@gmail.com>
2025-04-22 10:35:09 +0530

    deployed on firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import Body from "./components/Body.jsx";
-import Login from "./components/Login.jsx";
-import {Outlet} from "react-router-dom";
 import Header from "./components/Header.jsx"; // Import the Login component
 
 function App() {
