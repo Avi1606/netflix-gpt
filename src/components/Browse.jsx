bda8241e17e26639b2fd55f369287b75e04f6ea8
Avikumar <aviiiprajapati16@gmail.com>
2025-04-23 12:09:20 +0530

    logout button created

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -4,8 +4,13 @@ import Header from "./Header.jsx";
 const Browse = () => {
     return (
         <div>
-            <Header />
-            
+            <div className="flex">
+                <Header/>
+            </div>
+           <div className="flex justify-end">
+                <button className="text-white p-3 m-4 bg-red-600 flex flex-row rounded-xl cursor-pointer">Logout</button>
+            </div>
+
         </div>
     );
 };
