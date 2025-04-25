8bb5e8a985d950d914a835b925d9836ba4a79bf8
Avikumar <aviiiprajapati16@gmail.com>
2025-04-25 11:24:27 +0530

    custom hooks created

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,25 +1,9 @@
 import Header from "./Header";
-import {API_OPTIONS} from "../Utils/Constanats.jsx";
-import {useEffect} from "react";
-import {useDispatch} from "react-redux";
-import {addNowPlayingMovies} from "./moviesSlice.jsx";
+import useNowPlaying from "../hooks/useNowPlaying.jsx";
 
 const Browse = () => {
 
-    const dispatch = useDispatch();
-
-    const getNowPlayingMovies = async () => {
-        const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1',
-            API_OPTIONS);
-
-        const json = await data.json();
-        console.log(json);
-        dispatch(addNowPlayingMovies(json.results));
-    }
-
-    useEffect(() => {
-        getNowPlayingMovies();
-    }, []);
+     useNowPlaying();
 
     return (
         <div>
